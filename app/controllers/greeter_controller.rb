031453b3c52e18a5f42294a50a7677ef8fd13e57
Eyan-Green <ian.green736@btinternet.com>
2016-09-16 14:10:48 +0100

    add instance variables to greet controller

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,6 +1,12 @@
 class GreeterController < ApplicationController
   def hello
-  end
+  	random_names = ["Ian", "Angie", "Michael"]
+  	@name = random_names.sample
+  	@time = Time.now
+  	@times_displayed ||= 0
+  	@times_displayed += 1
+  end 
+end
   def goodbye
   end 
 end
